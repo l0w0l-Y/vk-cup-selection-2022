4c78b05ed51bd6addc5ec505065b3a7ce3308034
Aleksandra <79462005+l0w0l-Y@users.noreply.github.com>
2023-01-13 21:17:33 +0300

    Theme is updated

--- core-theme/src/main/java/com/kaleksandra/coretheme/Color.kt
+++ core-theme/src/main/java/com/kaleksandra/coretheme/Color.kt
@@ -1,15 +1,51 @@
 package com.kaleksandra.coretheme
 
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val primary = Color(0xFFFF5317)
-val onPrimary = Color(0xFFFFFFFF)
-val onBackground = Color(0xA3000000)
-val surface = Color(0xFFE6E6E6)
-val onSurface = Color(0xFF202020)
-val background = Color(0xFFFFFFFF)
+val primary = Color(0xFFA80404)
+val secondary = Color(0xFFE4EAF1)
+val correct = Color(0xFF4EB54F)
+val correctBackground = Color(0xFFA6EAB7)
+val uncorrect = Color(0xFFB63737)
+val uncorrectBackground = Color(0xFFE79191)
 
-val darkBackground = Color(0xFF000000)
-val darkOnBackground = Color(0x7AFFFFFF)
-val darkSurface = Color(0x2EFFFFFF)
-val darkOnSurface = Color(0xFFFFFFFF)
+@Immutable
+data class VKColors(
+    val primary: Color,
+    val secondary: Color,
+    val correct: Color,
+    val correctBackground: Color,
+    val uncorrect: Color,
+    val uncorrectBackground: Color,
+)
+
+val LocalVKColors = staticCompositionLocalOf { LightTheme }
+
+val LightTheme = VKColors(
+    primary = primary,
+    secondary = secondary,
+    correct = correct,
+    correctBackground = correctBackground,
+    uncorrect = uncorrect,
+    uncorrectBackground = uncorrectBackground,
+)
+
+val DarkTheme = VKColors(
+    primary = primary,
+    secondary = secondary,
+    correct = correct,
+    correctBackground = correctBackground,
+    uncorrect = uncorrect,
+    uncorrectBackground = uncorrectBackground,
+)
+
+internal val DarkColors = darkColorScheme(
+    primary = primary
+)
+internal val LightColors = lightColorScheme(
+    primary = primary
+)
